8cdac244130ee8df5c9742a55cdcde51016c61d9
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-09-13 13:03:51 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,77 @@
-keyboard.kbEvent(KeyValue.key1, function () {
-    最後出現數 += 1
+keyboard.kbEvent(KeyValue.keyminus, function () {
+    keyboard.setIndexColor(11, 0xff0000)
+    basic.pause(200)
+    keyboard.ledBlank()
+    I2C_LCD1602.ShowString("Class" + "     ", 4, 1)
 })
+keyboard.kbEvent(KeyValue.keydf, function () {
+    keyboard.setIndexColor(15, 0xff0000)
+    basic.pause(200)
+    keyboard.ledBlank()
+    I2C_LCD1602.clear()
+    I2C_LCD1602.ShowString("Put In Your", 4, 0)
+    I2C_LCD1602.ShowString("Homework", 4, 1)
+})
+let 最後出現數 = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
-let 最後出現數 = 70100
-I2C_LCD1602.ShowNumber(最後出現數, 0, 0)
-I2C_LCD1602.ShowString("Hello", 0, 0)
+I2C_LCD1602.ShowString("Enter The Class", 0, 0)
+I2C_LCD1602.ShowString("Class" + " " + 最後出現數, 4, 1)
+basic.forever(function () {
+    if (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || (keyboard.keyPressed(KeyValue.key4) || (keyboard.keyPressed(KeyValue.key5) || (keyboard.keyPressed(KeyValue.key6) || (keyboard.keyPressed(KeyValue.key7) || (keyboard.keyPressed(KeyValue.key8) || keyboard.keyPressed(KeyValue.key9)))))))))) {
+        最後出現數 = keyboard.keyMathNumber()
+        I2C_LCD1602.ShowString("Class" + " " + 最後出現數, 4, 1)
+    }
+})
+basic.forever(function () {
+    if (keyboard.keyPressed(KeyValue.key0)) {
+        keyboard.setIndexColor(0, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key1)) {
+        keyboard.setIndexColor(1, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key2)) {
+        keyboard.setIndexColor(2, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key3)) {
+        keyboard.setIndexColor(3, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key4)) {
+        keyboard.setIndexColor(4, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key5)) {
+        keyboard.setIndexColor(5, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key6)) {
+        keyboard.setIndexColor(6, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key7)) {
+        keyboard.setIndexColor(7, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key8)) {
+        keyboard.setIndexColor(8, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+    if (keyboard.keyPressed(KeyValue.key9)) {
+        keyboard.setIndexColor(9, 0xff0000)
+        basic.pause(200)
+        keyboard.ledBlank()
+    }
+})
