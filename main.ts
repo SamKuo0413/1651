6a64fa26de0a73f8557d403bf84a95540b3c8e08
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-09-13 13:15:35 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,25 +2,26 @@ keyboard.kbEvent(KeyValue.keyminus, function () {
     keyboard.setIndexColor(11, 0xff0000)
     basic.pause(200)
     keyboard.ledBlank()
-    I2C_LCD1602.ShowString("Class" + "     ", 4, 1)
+    I2C_LCD1602.ShowString("Class" + "               ", 5, 1)
 })
 keyboard.kbEvent(KeyValue.keydf, function () {
     keyboard.setIndexColor(15, 0xff0000)
     basic.pause(200)
     keyboard.ledBlank()
     I2C_LCD1602.clear()
-    I2C_LCD1602.ShowString("Put In Your", 4, 0)
-    I2C_LCD1602.ShowString("Homework", 4, 1)
+    I2C_LCD1602.ShowString("Put In Your", 2, 0)
+    I2C_LCD1602.ShowString("Homework", 5, 1)
 })
+let 超音波距離 = 0
 let 最後出現數 = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
 I2C_LCD1602.ShowString("Enter The Class", 0, 0)
-I2C_LCD1602.ShowString("Class" + " " + 最後出現數, 4, 1)
+I2C_LCD1602.ShowString("Class" + " " + 最後出現數, 5, 1)
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || (keyboard.keyPressed(KeyValue.key4) || (keyboard.keyPressed(KeyValue.key5) || (keyboard.keyPressed(KeyValue.key6) || (keyboard.keyPressed(KeyValue.key7) || (keyboard.keyPressed(KeyValue.key8) || keyboard.keyPressed(KeyValue.key9)))))))))) {
         最後出現數 = keyboard.keyMathNumber()
-        I2C_LCD1602.ShowString("Class" + " " + 最後出現數, 4, 1)
+        I2C_LCD1602.ShowString("Class" + " " + 最後出現數, 5, 1)
     }
 })
 basic.forever(function () {
@@ -74,4 +75,13 @@ basic.forever(function () {
         basic.pause(200)
         keyboard.ledBlank()
     }
+})
+basic.forever(function () {
+    超音波距離 = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    I2C_LCD1602.ShowNumber(超音波距離, 0, 1)
+    basic.pause(2000)
 })
