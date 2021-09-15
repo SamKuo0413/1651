609da093611aa1fa4b9000c2dd66acc7d64d795e
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-09-15 13:14:10 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ keyboard.kbEvent(KeyValue.keyminus, function () {
     keyboard.ledBlank()
     I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
 })
+input.onButtonPressed(Button.A, function () {
+    科目 += 1
+})
 keyboard.kbEvent(KeyValue.keydf, function () {
     keyboard.setIndexColor(15, 0xff0000)
     basic.pause(200)
@@ -25,13 +28,28 @@ keyboard.kbEvent(KeyValue.keydf, function () {
         basic.pause(200)
     }
 })
+let 最後出現數 = 0
 let 距離2 = 0
 let 距離1 = 0
-let 最後出現數 = 0
+let 科目 = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
 I2C_LCD1602.ShowString("Enter The Code", 1, 0)
-I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
+if (科目 == 0) {
+    I2C_LCD1602.ShowString("Chinese", 0, 1)
+}
+if (科目 == 1) {
+    I2C_LCD1602.ShowString("Math", 0, 1)
+}
+if (科目 == 2) {
+    I2C_LCD1602.ShowString("English", 0, 1)
+}
+if (科目 == 3) {
+    I2C_LCD1602.ShowString("Science", 0, 1)
+}
+if (科目 == 4) {
+    I2C_LCD1602.ShowString("Social studies", 0, 1)
+}
 距離1 = sonar.ping(
 DigitalPin.P0,
 DigitalPin.P1,
@@ -40,8 +58,16 @@ PingUnit.Centimeters
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || (keyboard.keyPressed(KeyValue.key4) || (keyboard.keyPressed(KeyValue.key5) || (keyboard.keyPressed(KeyValue.key6) || (keyboard.keyPressed(KeyValue.key7) || (keyboard.keyPressed(KeyValue.key8) || keyboard.keyPressed(KeyValue.key9)))))))))) {
         最後出現數 = keyboard.keyMathNumber()
+        I2C_LCD1602.clear()
+        I2C_LCD1602.ShowString("Chinese", 4, 0)
         I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
     }
+})
+basic.forever(function () {
+    if (最後出現數 > 99999) {
+        I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
+    }
+    basic.pause(1500)
 })
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0)) {
@@ -102,13 +128,4 @@ basic.forever(function () {
     PingUnit.Centimeters
     )
     basic.pause(2000)
-})
-basic.forever(function () {
-    if (最後出現數 > 99999) {
-        I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
-    }
-    basic.pause(2000)
-})
-basic.forever(function () {
-	
 })
