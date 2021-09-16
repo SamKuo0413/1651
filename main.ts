b03109f39121151d1f5f6b28adebcb2c3cbbb078
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-09-16 13:09:55 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,6 @@ keyboard.kbEvent(KeyValue.keyminus, function () {
     keyboard.ledBlank()
     I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
 })
-input.onButtonPressed(Button.A, function () {
-    科目 += 1
-})
 keyboard.kbEvent(KeyValue.keydf, function () {
     keyboard.setIndexColor(15, 0xff0000)
     basic.pause(200)
@@ -14,47 +11,13 @@ keyboard.kbEvent(KeyValue.keydf, function () {
     I2C_LCD1602.clear()
     I2C_LCD1602.ShowString("Put In Your", 3, 0)
     I2C_LCD1602.ShowString("Homework", 4, 1)
-    for (let index = 0; index < 4; index++) {
-        if (距離1 > 距離2) {
-            I2C_LCD1602.clear()
-            距離1 = sonar.ping(
-            DigitalPin.P0,
-            DigitalPin.P1,
-            PingUnit.Centimeters
-            )
-            I2C_LCD1602.ShowString("Enter The Code", 1, 0)
-            I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
-        }
-        basic.pause(200)
-    }
 })
 let 最後出現數 = 0
-let 距離2 = 0
-let 距離1 = 0
 let 科目 = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
 I2C_LCD1602.ShowString("Enter The Code", 1, 0)
-if (科目 == 0) {
-    I2C_LCD1602.ShowString("Chinese", 0, 1)
-}
-if (科目 == 1) {
-    I2C_LCD1602.ShowString("Math", 0, 1)
-}
-if (科目 == 2) {
-    I2C_LCD1602.ShowString("English", 0, 1)
-}
-if (科目 == 3) {
-    I2C_LCD1602.ShowString("Science", 0, 1)
-}
-if (科目 == 4) {
-    I2C_LCD1602.ShowString("Social studies", 0, 1)
-}
-距離1 = sonar.ping(
-DigitalPin.P0,
-DigitalPin.P1,
-PingUnit.Centimeters
-)
+I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || (keyboard.keyPressed(KeyValue.key4) || (keyboard.keyPressed(KeyValue.key5) || (keyboard.keyPressed(KeyValue.key6) || (keyboard.keyPressed(KeyValue.key7) || (keyboard.keyPressed(KeyValue.key8) || keyboard.keyPressed(KeyValue.key9)))))))))) {
         最後出現數 = keyboard.keyMathNumber()
@@ -120,12 +83,4 @@ basic.forever(function () {
         basic.pause(200)
         keyboard.ledBlank()
     }
-})
-basic.forever(function () {
-    距離2 = sonar.ping(
-    DigitalPin.P0,
-    DigitalPin.P1,
-    PingUnit.Centimeters
-    )
-    basic.pause(2000)
 })
