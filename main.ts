d131ea3d1447b653121f0ebfd9ec17786b2d6834
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-09-17 13:16:46 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,18 +11,62 @@ keyboard.kbEvent(KeyValue.keydf, function () {
     I2C_LCD1602.clear()
     I2C_LCD1602.ShowString("Put In Your", 3, 0)
     I2C_LCD1602.ShowString("Homework", 4, 1)
+    距離二 = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P0,
+    PingUnit.Centimeters
+    )
+    if (距離一 > 距離二) {
+        if (科目 == 0) {
+            let 國文老師的物聯網密碼 = ""
+            ESP8266_IoT.setData(
+            國文老師的物聯網密碼,
+            最後出現數座號
+            )
+        }
+        if (科目 == 1) {
+            let 數學老師的物聯網密碼 = ""
+            ESP8266_IoT.setData(
+            數學老師的物聯網密碼,
+            最後出現數座號
+            )
+        }
+        if (科目 == 2) {
+            let 英文老師的物聯網密碼 = ""
+            ESP8266_IoT.setData(
+            英文老師的物聯網密碼,
+            最後出現數座號
+            )
+        }
+        科目 = 0
+        距離一 = sonar.ping(
+        DigitalPin.P0,
+        DigitalPin.P0,
+        PingUnit.MicroSeconds
+        )
+        最後出現數座號 = 0
+    }
 })
-let 最後出現數 = 0
+let 距離二 = 0
+let 距離一 = 0
+let 科目 = 0
+let 最後出現數座號 = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
 I2C_LCD1602.ShowString("Enter The Code", 1, 0)
-I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
+I2C_LCD1602.ShowString("Code" + " " + 最後出現數座號, 3, 1)
+科目 = 0
+距離一 = sonar.ping(
+DigitalPin.P0,
+DigitalPin.P0,
+PingUnit.Centimeters
+)
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || (keyboard.keyPressed(KeyValue.key4) || (keyboard.keyPressed(KeyValue.key5) || (keyboard.keyPressed(KeyValue.key6) || (keyboard.keyPressed(KeyValue.key7) || (keyboard.keyPressed(KeyValue.key8) || keyboard.keyPressed(KeyValue.key9)))))))))) {
-        最後出現數 = keyboard.keyMathNumber()
-        I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
+        最後出現數座號 = keyboard.keyMathNumber()
+        I2C_LCD1602.ShowString("Code" + " " + 最後出現數座號, 3, 1)
     }
-    if (最後出現數 > 99999) {
+    if (最後出現數座號 > 99999) {
         I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
     }
     basic.pause(1500)
