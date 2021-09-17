8796978cbf573a61abe3c3437e958d6604b2a5aa
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-09-17 13:11:58 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,8 +15,24 @@ keyboard.kbEvent(KeyValue.keydf, function () {
 let 最後出現數 = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
-I2C_LCD1602.ShowString("Enter The Code", 1, 0)
-I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
+I2C_LCD1602.ShowString("Enter The Subject", 1, 0)
+while (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || keyboard.keyPressed(KeyValue.key4))))) {
+    if (keyboard.keyPressed(KeyValue.key0)) {
+        I2C_LCD1602.ShowString("Chinese", 0, 1)
+    }
+    if (keyboard.keyPressed(KeyValue.key1)) {
+        I2C_LCD1602.ShowString("English", 0, 1)
+    }
+    if (keyboard.keyPressed(KeyValue.key2)) {
+        I2C_LCD1602.ShowString("Math", 0, 1)
+    }
+    if (keyboard.keyPressed(KeyValue.key3)) {
+        I2C_LCD1602.ShowString("Science", 0, 1)
+    }
+    if (keyboard.keyPressed(KeyValue.key4)) {
+        I2C_LCD1602.ShowString("Social studies", 0, 1)
+    }
+}
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || (keyboard.keyPressed(KeyValue.key4) || (keyboard.keyPressed(KeyValue.key5) || (keyboard.keyPressed(KeyValue.key6) || (keyboard.keyPressed(KeyValue.key7) || (keyboard.keyPressed(KeyValue.key8) || keyboard.keyPressed(KeyValue.key9)))))))))) {
         最後出現數 = keyboard.keyMathNumber()
@@ -26,15 +42,6 @@ basic.forever(function () {
         I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
     }
     basic.pause(1500)
-    if (keyboard.keyPressed(KeyValue.key0)) {
-    	
-    }
-    if (keyboard.keyPressed(KeyValue.key0)) {
-    	
-    }
-    if (keyboard.keyPressed(KeyValue.key0)) {
-    	
-    }
 })
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0)) {
