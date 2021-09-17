9ae76f5a0b3bf585e211cb47728e7e31280a6438
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-09-17 13:00:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,6 @@ keyboard.kbEvent(KeyValue.keydf, function () {
     I2C_LCD1602.ShowString("Homework", 4, 1)
 })
 let 最後出現數 = 0
-let 科目 = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
 I2C_LCD1602.ShowString("Enter The Code", 1, 0)
@@ -21,16 +20,12 @@ I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || (keyboard.keyPressed(KeyValue.key4) || (keyboard.keyPressed(KeyValue.key5) || (keyboard.keyPressed(KeyValue.key6) || (keyboard.keyPressed(KeyValue.key7) || (keyboard.keyPressed(KeyValue.key8) || keyboard.keyPressed(KeyValue.key9)))))))))) {
         最後出現數 = keyboard.keyMathNumber()
-        I2C_LCD1602.clear()
-        I2C_LCD1602.ShowString("Chinese", 4, 0)
         I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
     }
     if (最後出現數 > 99999) {
         I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
     }
     basic.pause(1500)
-})
-basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0)) {
         keyboard.setIndexColor(0, 0xff0000)
         basic.pause(200)
@@ -81,7 +76,4 @@ basic.forever(function () {
         basic.pause(200)
         keyboard.ledBlank()
     }
-})
-basic.forever(function () {
-	
 })
