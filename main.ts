afac4f9841159ccaf79269b7a7028a499d9cf8d0
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-09-24 15:15:36 +0800

    還原「2021/9/14 下午1:11:55 更新 main.blocks, main.ts」

--- main.ts
+++ main.ts
@@ -11,73 +11,35 @@ keyboard.kbEvent(KeyValue.keydf, function () {
     I2C_LCD1602.clear()
     I2C_LCD1602.ShowString("Put In Your", 3, 0)
     I2C_LCD1602.ShowString("Homework", 4, 1)
-    距離二 = sonar.ping(
-    DigitalPin.P0,
-    DigitalPin.P0,
-    PingUnit.Centimeters
-    )
-    if (距離一 > 距離二) {
-        if (科目 == 0) {
-            let 國文老師的物聯網密碼 = ""
-            ESP8266_IoT.setData(
-            國文老師的物聯網密碼,
-            最後出現數座號
+    for (let index = 0; index < 4; index++) {
+        if (距離1 > 距離2) {
+            距離1 = sonar.ping(
+            DigitalPin.P0,
+            DigitalPin.P1,
+            PingUnit.Centimeters
             )
+            I2C_LCD1602.ShowString("Enter The Code", 1, 0)
+            I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
         }
-        if (科目 == 1) {
-            let 數學老師的物聯網密碼 = ""
-            ESP8266_IoT.setData(
-            數學老師的物聯網密碼,
-            最後出現數座號
-            )
-        }
-        if (科目 == 2) {
-            let 英文老師的物聯網密碼 = ""
-            ESP8266_IoT.setData(
-            英文老師的物聯網密碼,
-            最後出現數座號
-            )
-        }
-        科目 = 0
-        距離一 = sonar.ping(
-        DigitalPin.P0,
-        DigitalPin.P0,
-        PingUnit.MicroSeconds
-        )
-        最後出現數座號 = 0
+        basic.pause(200)
     }
 })
-let 距離二 = 0
-let 距離一 = 0
-let 科目 = 0
-let 最後出現數座號 = 0
+let 距離2 = 0
+let 距離1 = 0
+let 最後出現數 = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
 I2C_LCD1602.ShowString("Enter The Code", 1, 0)
-I2C_LCD1602.ShowString("Code" + " " + 最後出現數座號, 3, 1)
-科目 = 0
-距離一 = sonar.ping(
+I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
+距離1 = sonar.ping(
 DigitalPin.P0,
-DigitalPin.P0,
+DigitalPin.P1,
 PingUnit.Centimeters
 )
 basic.forever(function () {
     if (keyboard.keyPressed(KeyValue.key0) || (keyboard.keyPressed(KeyValue.key1) || (keyboard.keyPressed(KeyValue.key2) || (keyboard.keyPressed(KeyValue.key3) || (keyboard.keyPressed(KeyValue.key4) || (keyboard.keyPressed(KeyValue.key5) || (keyboard.keyPressed(KeyValue.key6) || (keyboard.keyPressed(KeyValue.key7) || (keyboard.keyPressed(KeyValue.key8) || keyboard.keyPressed(KeyValue.key9)))))))))) {
-        最後出現數座號 = keyboard.keyMathNumber()
-        I2C_LCD1602.ShowString("Code" + " " + 最後出現數座號, 3, 1)
-    }
-    if (最後出現數座號 > 99999) {
-        I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
-    }
-    basic.pause(1500)
-    if (keyboard.keyPressed(KeyValue.key0)) {
-    	
-    }
-    if (keyboard.keyPressed(KeyValue.key0)) {
-    	
-    }
-    if (keyboard.keyPressed(KeyValue.key0)) {
-    	
+        最後出現數 = keyboard.keyMathNumber()
+        I2C_LCD1602.ShowString("Code" + " " + 最後出現數, 3, 1)
     }
 })
 basic.forever(function () {
@@ -131,4 +93,21 @@ basic.forever(function () {
         basic.pause(200)
         keyboard.ledBlank()
     }
+})
+basic.forever(function () {
+    距離2 = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    basic.pause(2000)
+})
+basic.forever(function () {
+    if (最後出現數 > 99999) {
+        I2C_LCD1602.ShowString("Code" + "               ", 3, 1)
+    }
+    basic.pause(2000)
+})
+basic.forever(function () {
+	
 })
